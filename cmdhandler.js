9100a1dc34eb154bc586c101dee4d74d8c701943
Michał <38110165+Michal2SAB@users.noreply.github.com>
2021-10-19 01:14:53 +0200

    Create cmdhandler.js

---
+++ cmdhandler.js
@@ -0,0 +1,27 @@
+// I'm going to handle commands here
+
+const btc = require('./commands/cryptocurrency.js');
+const eth = btc;
+const luck = require('./commands/luck.js');
+const calc = require('./commands/calc.js');
+const bnews = require('./commands/bnews.js');
+const ask = require('./commands/ask.js');
+const mw = require('./commands/mweather.js');
+const apod = require('./commands/apod.js');
+
+const config = require('./config.json');
+const prefix = config.prefix;
+
+const cmds = { btc, eth, luck, calc, bnews, ask, mw, apod}
+
+module.exports = async function (msg) {
+    if (msg.author.bot) return;
+    if (msg.content.startsWith(prefix)) {
+        const [cmd, ...args] = msg.content
+            .trim()
+            .substring(prefix.length)
+            .split(/\s+/);
+        
+        cmds[cmd](msg, args, cmd);
+    }
+};
