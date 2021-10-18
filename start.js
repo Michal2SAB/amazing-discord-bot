8f20feefa955ca512f7cbee161d6f84c5442ee65
Michał <38110165+Michal2SAB@users.noreply.github.com>
2021-10-19 01:13:20 +0200

    Create start.js

---
+++ start.js
@@ -0,0 +1,16 @@
+// this is the main code that will get the bot logged in, setup, etc.
+
+const { Client } = require('discord.js');
+const client = new Client();
+
+const config = require('./config.json');
+const CMDHandler = require('./cmdhandler.js');
+
+client.on('ready', () => {
+    client.user.setActivity(config.activity, { type: config.activityType });
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', CMDHandler);
+
+client.login(process.env.bot_token); // or config.token if running locally.
