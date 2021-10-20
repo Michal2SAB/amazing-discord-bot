15a477422e53d85ec543d4b83c432eedb5b41b0e
Michał <38110165+Michal2SAB@users.noreply.github.com>
2021-10-20 15:35:14 +0200

    Update cmdhandler.js
    Added new 'soon' command which is work in progress and will be coming soon. It's for predicting bitcoin price in period of time (max 1 in week) and earning win or loss score for it when the prediction date comes.

--- cmdhandler.js
+++ cmdhandler.js
@@ -9,11 +9,12 @@ const ask = require('./commands/ask.js');
 const mweather = require('./commands/mweather.js');
 const apod = require('./commands/apod.js');
 const ytags = require('./commands/ytags.js');
+const soon = require('./commands/soon.js');
 
 const config = require('./config.json');
 const prefix = config.prefix;
 
-const cmds = { btc, eth, luck, calc, bnews, ask, mweather, apod, ytags}
+const cmds = { btc, eth, luck, calc, bnews, ask, mweather, apod, ytags, soon}
 
 module.exports = async function (msg) {
     if (msg.author.bot) return;
