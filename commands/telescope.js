013aa30f88d5505940a67c7e76bf825b9aa56073
Michał <38110165+Michal2SAB@users.noreply.github.com>
2021-12-22 17:50:51 +0100

    Update telescope.js

--- commands/telescope.js
+++ commands/telescope.js
@@ -7,7 +7,7 @@ module.exports = async function (msg) {
     try {
         msg.channel.send("Loading NASA's content..")
         const browser = await puppeteer.launch({
-            args: ["--proxy-server='direct://'", '--proxy-bypass-list=*'],
+            args: ["--proxy-server='direct://'", '--proxy-bypass-list=*', '--no-sandbox', '--disable-setuid-sandbox'],
             headless: true
         });
         const [page] = await browser.pages();
