194987a420a60c0151e37a61d9bb3bf9c76f9ec6
Michał <38110165+Michal2SAB@users.noreply.github.com>
2021-12-28 17:37:11 +0100

    Update cmdhandler.js

--- cmdhandler.js
+++ cmdhandler.js
@@ -15,11 +15,12 @@ const telescope = require('./commands/telescope.js');
 const ytags = require('./commands/ytags.js');
 const soon = require('./commands/soon.js');
 const colvert = require('./commands/colvert.js');
+const help = require('./commands/help.js');
 
 const config = require('./config.json');
 const prefix = config.prefix;
 
-const cmds = { btc, eth, luck, ethluck, calc, bnews, ask, mweather, mars, marsnews, apod, telescope, ytags, soon, colvert}
+const cmds = { btc, eth, luck, ethluck, calc, bnews, ask, mweather, mars, marsnews, apod, telescope, ytags, soon, colvert, help}
 
 module.exports = async function (msg) {
     if (msg.author.bot) return;
