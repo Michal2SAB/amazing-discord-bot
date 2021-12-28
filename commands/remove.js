e47d2b1ac2019f497677b887dcdb72aefb5ae023
Michał <38110165+Michal2SAB@users.noreply.github.com>
2021-12-28 18:54:19 +0100

    Update remove.js

--- commands/remove.js
+++ commands/remove.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 const fs = require('fs');
 
-module.exports = async function (msg, url) {
+module.exports = function (msg, url) {
     axios({
         method: 'post',
         url: 'https://api.remove.bg/v1.0/removebg',
@@ -15,10 +15,9 @@ module.exports = async function (msg, url) {
     .then((response) => {
         if(response.status != 200) return console.error('Error:', response.status, response.statusText);
         fs.writeFileSync("no-bg.png", response.data);
+        msg.channel.send("Removed background from your image:", {files: ["no-bg.png"]});
     })
     .catch((error) => {
         return msg.reply('Request failed! Try a different url or try again.');
     });
-    await msg.channel.send("Removed background from your image:", {files: ["no-bg.png"]});
-    fs.unlinkSync("no-bg.png");
 };
