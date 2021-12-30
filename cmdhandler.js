97b5648fb7c3c7c7708ac23dfe7c25c00cb2de87
Michał <38110165+Michal2SAB@users.noreply.github.com>
2021-12-30 21:16:51 +0100

    Update cmdhandler.js
    Added new admin command 'status'

--- cmdhandler.js
+++ cmdhandler.js
@@ -17,12 +17,13 @@ const soon = require('./commands/soon.js');
 const colvert = require('./commands/colvert.js');
 const help = require('./commands/help.js');
 const remove = require('./commands/remove.js');
+const status = require('./commands/status.js');
 
 const config = require('./config.json');
 const prefix = config.prefix;
 
 const cmds = { btc, eth, luck, ethluck, calc, bnews, ask, mweather, mars, marsnews, apod, 
-              telescope, ytags, soon, colvert, help, remove}
+              telescope, ytags, soon, colvert, help, remove, status}
 
 module.exports = async function (msg) {
     if (msg.author.bot) return;
