6cf61ea7e37fbefd8b91cf04cf0623d4e730cdaa
Michał <38110165+Michal2SAB@users.noreply.github.com>
2022-01-15 19:06:43 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,3 +1,3 @@
 module.exports = function (msg) {
-  msg.channel.send("Help yourself");
+  msg.channel.send("Hi. I'm a discord bot written by Michal2SAB and I do everything you can imagine. Type !commands for list of all commands.");
 }
